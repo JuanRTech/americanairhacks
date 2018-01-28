94da5bc3817a7657814331a9d25c730f47a67df3
Tmanpenguin <taylor_schorlemmer@yahoo.com>
2018-01-28 00:40:53 -0600

    Orders

--- attendant/orders.php
+++ attendant/orders.php
@@ -1,5 +1,72 @@
 <?php require('header.php');?>
   <div class="container-fluid">
-    
+    <div class="row">
+      <div class="col">
+        <h1>Orders</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <div class="row">
+              <div class='col'>
+                <b>Seat: 13A</b>
+              </div>
+              <div class='col'>
+                <b>Items: Coke</b>
+              </div>
+              <div class='col'>
+                <b>Price: 5.00</b>
+              </div>
+              <div class='col'>
+                <b>4 mins ago</b>
+              </div>
+              <div class='col'>
+                <button type="button" class="btn btn-success btn-block">Delivered</button>
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class='col'>
+                <b>Seat: 4C</b>
+              </div>
+              <div class='col'>
+                <b>Items: Coke</b>
+              </div>
+              <div class='col'>
+                <b>Price: 5.00</b>
+              </div>
+              <div class='col'>
+                <b>3 mins ago</b>
+              </div>
+              <div class='col'>
+                <button type="button" class="btn btn-success btn-block">Delivered</button>
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class='col'>
+                <b>Seat: 8A</b>
+              </div>
+              <div class='col'>
+                <b>Items: Coke</b>
+              </div>
+              <div class='col'>
+                <b>Price: 5.00</b>
+              </div>
+              <div class='col'>
+                <b>1 mins ago</b>
+              </div>
+              <div class='col'>
+                <button type="button" class="btn btn-success btn-block">Delivered</button>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 <?php require('footer.php');?>

7b746197f2367957404cb9a995e682748973fb47
Juan Requena <14365617+JuanRTech@users.noreply.github.com>
2018-01-28 05:02:26 -0600

    Added zendesk

--- menu.php
+++ menu.php
@@ -23,7 +23,15 @@
 
     <!-- Custom styles for this template -->
     <link href="css/freelancer.css" rel="stylesheet">
-
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?5RbZbbJDIOHTpFbNN4BJ34Zw3i7JJTBS";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
   </head>
 
   <body id="page-top">

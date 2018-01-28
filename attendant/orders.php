90083efabbf4879b51baad2fea6c5068395635f2
Tmanpenguin <taylor_schorlemmer@yahoo.com>
2018-01-28 08:38:19 -0600

    Orders

--- attendant/orders.php
+++ attendant/orders.php
@@ -1,4 +1,20 @@
 <?php require('header.php');?>
+  <?php
+    require('sqlsetup.php');
+    $sql = "Select orders.*, items.Name, items.Price from orders, items where (orders.Complete = false and orders.ItemID = items.ID) ORDER BY timediff(orders.Ordered,CURRENT_TIMESTAMP)";
+    $result = $conn->query($sql);
+    $conn->close();
+
+    function CompleteOrder($orderID){
+      include('sqlsetup.php');
+      $updatesql = "UPDATE orders\n"
+      . "    SET `Complete` = 1,\n"
+      . "        `Delivered` = CURRENT_TIMESTAMP\n"
+      . "    WHERE `ID` = " . $orderID;
+      $conn->query($updatesql);
+      $conn->close();
+    }
+  ?>
   <div class="container-fluid">
     <div class="row">
       <div class="col">
@@ -8,65 +24,38 @@
     <div class="row">
       <div class="col-md-12">
         <ul class="list-group">
-          <li class="list-group-item">
-            <div class="row">
-              <div class='col'>
-                <b>Seat: 13A</b>
-              </div>
-              <div class='col'>
-                <b>Items: Coke</b>
-              </div>
-              <div class='col'>
-                <b>Price: 5.00</b>
-              </div>
-              <div class='col'>
-                <b>4 mins ago</b>
-              </div>
-              <div class='col'>
-                <button type="button" class="btn btn-success btn-block">Delivered</button>
-              </div>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="row">
-              <div class='col'>
-                <b>Seat: 4C</b>
-              </div>
-              <div class='col'>
-                <b>Items: Coke</b>
-              </div>
-              <div class='col'>
-                <b>Price: 5.00</b>
-              </div>
-              <div class='col'>
-                <b>3 mins ago</b>
-              </div>
-              <div class='col'>
-                <button type="button" class="btn btn-success btn-block">Delivered</button>
-              </div>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="row">
-              <div class='col'>
-                <b>Seat: 8A</b>
-              </div>
-              <div class='col'>
-                <b>Items: Coke</b>
-              </div>
-              <div class='col'>
-                <b>Price: 5.00</b>
-              </div>
-              <div class='col'>
-                <b>1 mins ago</b>
-              </div>
-              <div class='col'>
-                <button type="button" class="btn btn-success btn-block">Delivered</button>
-              </div>
-            </div>
-          </li>
+          <?php
+            while($res = mysqli_fetch_assoc($result)){
+              echo '<li class="list-group-item">';
+              echo '<div class="row">';
+              echo "<div class='col'>";
+              echo "<b>Seat: " . $res['Seat'] . "</b>";
+              echo "</div>";
+              echo "<div class='col'>";
+              echo "<b>Type: " . $res['Name'] . "</b>";
+              echo "</div>";
+              echo "<div class='col'>";
+              echo "<b>Price: " . $res['Price'] . "</b>";
+              echo "</div>";
+              echo "<div class='col'>";
+              echo "<b>Quantity: " . $res['Quantity'] . "</b>";
+              echo "</div>";
+              echo "<div class='col'>";
+              echo '<form action="orders.php" method="get">';
+              echo '<input type="hidden" name="act" value="' . $res["ID"] . '">';
+              echo '<button type="submit" class="btn btn-success btn-block">Delivered</button>';
+              echo '</form>';
+              echo '</div>';
+              echo '</div>';
+              echo '</li>';
+            }
+          ?>
         </ul>
       </div>
     </div>
   </div>
 <?php require('footer.php');?>
+<?php if (!empty($_GET['act'])) {
+    CompleteOrder($_GET['act']);
+  }
+  ?>
